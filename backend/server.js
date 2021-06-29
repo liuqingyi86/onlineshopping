b3bd6bdc5048e002f1a774d0515a9c9767f94274
Mike <mikeliu826@gmail.com>
2021-06-29 13:06:27 +1000

    config for the production environment

--- backend/server.js
+++ backend/server.js
@@ -41,9 +41,16 @@ const __dirname = path.resolve(); // return current folder
 //console.log(__dirname);
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.get("/", (req, res) => {
-  res.send("Server is ready!");
+// serve the react file
+app.use(express.static(path.join(__dirname, "/frontend/bulid")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
+
+// this code is just for the development and local server
+// app.get("/", (req, res) => {
+//   res.send("Server is ready!");
+// });
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
